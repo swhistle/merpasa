4ca0347d5ba0e058c6bc6fc318926d3ac236468e
Alexander Svistunov <alexsvistunov73@gmail.com>
2017-09-10 19:13:21 +0300

    create inner pages, add function changing url with ajax

---
+++ app/components/service/service.js
@@ -0,0 +1,25 @@
+(function() {
+  //переход по ссылке
+  var linkPage = document.querySelectorAll(".js-link");
+
+  linkPage.forEach(function(link, i) {
+    link.addEventListener("click", function(evt) {
+      evt.preventDefault();
+
+      // ajax запрос
+      var xhr = new XMLHttpRequest();
+      var url;
+
+      xhr.open('GET', 'data.json');
+
+      xhr.timeout = 10000;
+
+      xhr.onload = function(evt) {
+        url = JSON.parse(evt.srcElement.response);
+        window.location.replace(url[i].url);
+      }
+
+      xhr.send();
+    });
+  });
+})();
